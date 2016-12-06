8129689309823ebafdf8c88853690217ded26d1a
s141065 <s141065@ICS844.icsuser.nagaokaut.ac.jp>
2016-12-06 13:39:39 +0900

    DataBase完了

--- WEB-INF/src/db/DataBaseProperty.java
+++ WEB-INF/src/db/DataBaseProperty.java
@@ -3,9 +3,9 @@ package db;
 public class DataBaseProperty {
 	static final public String DB_URL = "jdbc:sqlserver://mis.nagaokaut.ac.jp";
 
-	static final public String DB_NAME = "SeTeamC";
+	static final public String DB_NAME = "SeTeamC_Story06";
 
-	static final public String DB_USERNAME = "SeTeamC";
+	static final public String DB_USERNAME = "141065";
 
-	static final public String DB_PASSWORD = "SeTeamC";
+	static final public String DB_PASSWORD = "14106582";
 }
